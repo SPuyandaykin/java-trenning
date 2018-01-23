fc54bc6f486d2e8856ca65f129ad1bacd07f5a99
Sergey Puyandaykin <s.puyandaykin@javad.com>
2018-01-23 13:07:30 +0300

    app.getNavigationHelper().OpenHomePage() is added

--- addressbook-web-tests/src/test/java/TestCases1/EditFirstContactTest.java
+++ addressbook-web-tests/src/test/java/TestCases1/EditFirstContactTest.java
@@ -8,6 +8,7 @@ public class EditFirstContactTest extends TestBase {
 
     @Test
     public void testEditFirstContact(){
+        app.getNavigationHelper().OpenHomePage();
         if(!app.getContactHelper().isAnyContact())
             app.getContactHelper().NewContactAdd(new ContactNameData("sergey", "ivanov", "Java Corporation"),
                     new ContactPhoneData("+79031111111", "+79042222222"));
