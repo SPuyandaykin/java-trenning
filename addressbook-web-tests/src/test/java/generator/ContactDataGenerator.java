5923d0a4724a89e7dbcfa5547d72404947dabe8b
Sergey Puyandaykin <s.puyandaykin@javad.com>
2018-02-08 11:10:49 +0300

    Properly file closing

--- addressbook-web-tests/src/test/java/generator/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/generator/ContactDataGenerator.java
@@ -56,18 +56,18 @@ public class ContactDataGenerator {
         Gson gson = new GsonBuilder().setPrettyPrinting()
                 .excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
     private void saveAsXML(List<ContactData> contacts, File file) throws IOException {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
         String xml = xstream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private String getFileFormat(){
@@ -89,21 +89,19 @@ public class ContactDataGenerator {
     }
 
     private void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts) {
-
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n",
-                    contact.getContactName().getFirstName(),
-                    contact.getContactName().getLastName(),
-                    contact.getContactName().getCompany(),
-                    contact.getContactPhone().getPhoneHome(),
-                    contact.getContactPhone().getPhoneMobile(),
-                    contact.getContactPhone().getWorkMobile(),
-                    contact.getContactPhone().getEmail(),
-                    contact.getContactPhone().getAddress()
-            ));
-
+        try(Writer writer = new FileWriter(file)) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n",
+                        contact.getContactName().getFirstName(),
+                        contact.getContactName().getLastName(),
+                        contact.getContactName().getCompany(),
+                        contact.getContactPhone().getPhoneHome(),
+                        contact.getContactPhone().getPhoneMobile(),
+                        contact.getContactPhone().getWorkMobile(),
+                        contact.getContactPhone().getEmail(),
+                        contact.getContactPhone().getAddress()
+                ));
+            }
         }
-        writer.close();
     }
 }
